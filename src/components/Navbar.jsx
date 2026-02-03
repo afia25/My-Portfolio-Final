e7896f50d2f272fed5984215708ef00298bf0794
Afia Tasnim <77075953+afia25@users.noreply.github.com>
2026-02-03 12:07:33 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
       <img className="mx-12 w-12" src={logo} alt="logo" />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-     {/*  <a href="https://www.linkedin.com/in/afia-tasnim-2704a0384/"><FaLinkedin /></a>   */}
+      <a href="https://www.linkedin.com/in/afia-tasnim-2704a0384/"><FaLinkedin /></a>
       <a href="https://github.com/afia25"><FaGithub /></a> 
-      <a href="https://scholar.google.com/citations?user=gWzNUJYAAAAJ&hl=en&authuser=1"><SiGooglescholar /></a>
-      <a href="https://www.researchgate.net/profile/Afia-Tasnim-4"><FaResearchgate /></a>
+      {/*  <a href="https://scholar.google.com/citations?user=gWzNUJYAAAAJ&hl=en&authuser=1"><SiGooglescholar /></a>   */}
+      {/*  <a href="https://www.researchgate.net/profile/Afia-Tasnim-4"><FaResearchgate /></a>   */}
     </div>
   </nav>
 };
