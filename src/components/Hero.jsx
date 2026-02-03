f96eefeba5c91406e87ee6819c9fa9db3066409e
Afia Tasnim <77075953+afia25@users.noreply.github.com>
2026-02-03 12:12:38 +0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
                     initial="hidden" 
                     animate="visible"
                     className="mx-12 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
-                        AI Engineer 
+                        Aspiring Full-Stack Software Engineer 
                     </motion.span> 
                     <motion.p 
                     variants={container(1)} 
